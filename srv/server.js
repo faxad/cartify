29aca9da6cdf5ba29e27018db0f34be947487493
Fawad Qureshi <fawad@outlook.com>
2016-06-13 22:42:18 +0300

    added post action for adding item

--- srv/server.js
+++ srv/server.js
@@ -77,7 +77,6 @@ dispatcher.onGet("/item", function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     MongoClient.connect(mongoDBUrl, function(err, db) {
       assert.equal(null, err);
-      
       getItemById(db, url_parts.query.id, function() {
           db.close();
       });
@@ -87,86 +86,21 @@ dispatcher.onGet("/item", function(req, res) {
 
 // Dispatcher: Set item
 
-            // var body='';
-
-            // req.on('data', function (data) {
-
-            //     body +=data;
-
-            // });
-
-            // req.on('end',function(){
-
-            //     var POST =  qs.parse(body);
-
-            //     console.log(POST);
-
-            // });
-            
-//A sample GET request    
-dispatcher.onGet("/page1", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-
-    var url = 'mongodb://fawad:fawad@ds013574.mlab.com:13574/foxtaildb';
-    MongoClient.connect(mongoDBUrl, function(err, db) {
-      assert.equal(null, err);
-      console.log("Connected correctly to server.");
-      db.close();
-    });
-
-    res.end('Page One');
-});
-
-var insertDocument = function(db, callback) {
-   db.collection('items').insertOne({
-        "itemId": 1,
-        "itemName": "Leaf Rake",
-        "itemCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt condimentum turpis. Nullam eu nisi vitae dolor lacinia cursus. Maecenas feugiat risus vel ex egestas, vel semper tellus tincidunt. Nunc lacus magna, dictum quis eros sed, suscipit auctor massa. Maecenas malesuada dui nulla, id ultricies turpis fringilla in. Duis vitae nisl id felis bibendum pulvinar at at nibh. Fusce augue dui, blandit sit amet diam non, pulvinar convallis elit. Sed convallis ipsum ut augue tincidunt, sit amet viverra lorem eleifend. Morbi consequat dignissim nisl, vel tristique erat elementum eu. Fusce maximus tellus in ligula condimentum ultrices. Suspendisse id venenatis risus. Phasellus nec odio lacinia, hendrerit nisi ac, placerat lorem. Ut id facilisis eros. Sed eu semper enim, in placerat felis",
-        "price": 19.95,
-        "quantity": 5,
-        "starRating": 3.2,
-        "imageUrl": "http://placehold.it/320x150"
-    }, function(err, result) {
+var addItem = function(db, body, callback) {
+    db.collection('items').insertOne(JSON.parse(body), function(err, result) {
     assert.equal(err, null);
     console.log("Inserted a document into the items collection.");
     callback();
   });
 };
 
-var findDocument = function(db, callback) {
-   var cursor =db.collection('items').find({ "itemName": "Leaf Rake" });
-   cursor.each(function(err, doc) {
-      assert.equal(err, null);
-      if (doc != null) {
-         console.dir(doc);
-      } else {
-         callback();
-      }
-   });
-};
-
-//A sample GET request    
-dispatcher.onGet("/insert", function(req, res) {
+dispatcher.onPost("/insert", function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-
-    var url = 'mongodb://fawad:fawad@ds013574.mlab.com:13574/foxtaildb';
     MongoClient.connect(mongoDBUrl, function(err, db) {
       assert.equal(null, err);
-      // insertDocument(db, function() {
-      //     db.close();
-      // });
-      findDocument(db, function() {
+      insertDocument(db, req.body, function() {
           db.close();
       });
     });
-
-    res.end('Page One');
+    res.end('Done!');
 }); 
-
-//A sample POST request
-dispatcher.onPost("/post1", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Got Post Data');
-});
