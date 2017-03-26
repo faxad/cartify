1e2228bb20c92145fbe982acf52582ee1a00d0c0
Fawad Qureshi <fawad@outlook.com>
2017-03-26 22:47:57 +0300

    imported animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -26,6 +27,7 @@ import { CalendarModule, RatingModule } from 'primeng/primeng';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
