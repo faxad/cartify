d20dc70d603ea84d1032f426c6a1a7ca0bfb2205
Fawad Qureshi <fawad@outlook.com>
2017-12-16 15:24:44 +0300

    cleaned up cart component

--- src/app/cart/cart-item-list.component.ts
+++ src/app/cart/cart-item-list.component.ts
@@ -2,26 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { CartService } from '../core/services/cart.service';
-import { ShopService } from '../core/services/shop.service'
 import { ICartItem } from '../shared';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators'
 
 
 @Component({
-    templateUrl: './cart-item-list.component.html',
-    providers: [CartService, ShopService]
+    templateUrl: './cart-item-list.component.html'
 })
 export class ItemCartComponent implements OnInit {
     private userCartItems$: Observable<ICartItem[]>;
-    private showLoading = true;
 
     constructor(private router: Router, private cart: CartService) {}
 
     ngOnInit(): void {
-        this.userCartItems$ = this.cart.getCartItems().pipe(
-            tap(() => this.showLoading = false)
-        )
+        this.userCartItems$ = this.cart.getCartItems();
     }
 
     increaseQuantity(item: ICartItem): void {
