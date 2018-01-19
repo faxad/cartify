6a15850227ce9965e7db7fff2b1f1a3d9102da76
Fawad Qureshi <fawad@outlook.com>
2018-01-20 02:15:40 +0300

    set prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://secure-xpress-api.7e14.starter-us-west-2.openshiftapps.com/',
+  sentryDns: 'https://3124c504af6e4242b3162368396a11b6@sentry.io/265893'
 };
