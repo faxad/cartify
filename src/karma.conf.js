a243ba2a413ad9c93dc58f83e19bac1f64b95387
Fawad Qureshi <fawad@outlook.com>
2018-05-16 22:48:02 +0300

    post upgrade restructure, fixed broken test runner

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
@@ -7,32 +7,23 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-phantomjs-launcher'),
-      require('karma-spec-reporter')
+      require('karma-spec-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-    files: [
-      "./node_modules/babel-polyfill/dist/polyfill.js",
-      "./node_modules/jquery/dist/jquery.min.js",
-      "./node_modules/web-animations-js/web-animations.min.js",
-      
-    ],
-    preprocessors: {
-      
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: {
+      reports: {
         html: 'coverage',
-        lcovonly: './coverage/coverage.lcov'
+        lcovonly: '../coverage/coverage.lcov'
       }
     },
     angularCli: {
-      config: './angular-cli.json',
-      environment: 'dev'
+      config: '../angular.json',
+      environment: 'production'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
               ? ['spec', 'coverage-istanbul']
